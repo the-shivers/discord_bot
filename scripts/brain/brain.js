700c4c17ca810c35134aa981c2cbbdb460524414
Brian <brianross13@gmail.com>
2021-04-18 02:40:32 -0500

    brain meme

--- scripts/brain/brain.js
+++ scripts/brain/brain.js
@@ -11,12 +11,12 @@ const d = require('../drake/drake.js');
 
 
 const char_limits = {
-  "12": 46,
-  "14": 40,
-  "16": 35,
-  "18": 30,
-  "24": 24,
-  "36": 16,
+  "12": 43,
+  "14": 37,
+  "16": 32,
+  "18": 28,
+  "24": 22,
+  "36": 15,
 }
 
 
@@ -30,7 +30,7 @@ function determineFontSize(string) {
     return 16;
   } else if (string.length > 220) {
     return 18;
-  } else if (string.length > 90) {
+  } else if (string.length > 70) {
     return 24;
   } else {
     return 36;
@@ -43,23 +43,24 @@ function determineLines(w_arr, font_size_str) {
   let sum = 0;
   let line_str = "";
   for (let i = 0; i < w_arr.length; i++) {
-    line_str += w_arr[i] + " ";
     sum += 1 + w_arr[i].length;
     if (sum >= char_limits[font_size_str]) {
-      return_arr = return_arr.concat(line_str + "\n");
+      return_arr = return_arr.concat(line_str + "\n" + w_arr[i] + " ");
       line_str = "";
-      sum = 0;
+      sum = w_arr[i].length;
+    } else {
+      line_str += w_arr[i] + " ";
     }
   }
   return_arr = return_arr.concat(line_str + "\n");
   return return_arr;
 }
 
-function brainParameters(content) {
+function brainParameters(contents) {
   // returns array of key parameters, imagestr and height
-  if (content.split("|").length === 3) {
-    return [3"template_3.jpg", 655];
-  } else if (content.split("|").length === 4) {
+  if (contents.split("|").length === 3) {
+    return ["template_3.jpg", 655];
+  } else if (contents.split("|").length === 4) {
     return ["template_4.jpg", 883];
   } else {
     return ["template_5.jpg", 1099];
@@ -70,7 +71,7 @@ async function brain(msg, contents) {
   // Determine if entry was valid
   if (
     contents.includes("|") &&
-    content.split("|").length >= 3
+    contents.split("|").length >= 3 &&
     contents.length > 7
   ) {
     // First process the contents.
@@ -79,25 +80,21 @@ async function brain(msg, contents) {
     let img = bp[0];
     let height = bp[1];
     let parts = rem_contents.replace(" | ", "|").split("|");
-
-
-    let a_size = determineFontSize(a_part);
-    let a_processed = determineLines(a_part.split(' '), a_size.toString());
-    let b_size = determineFontSize(b_part);
-    let b_processed = determineLines(b_part.split(' '), b_size.toString());
-    // Create and fill canvas
-    const canvas = Canvas.createCanvas(701, 1066);
+    const canvas = Canvas.createCanvas(700, height);
     const ctx = canvas.getContext('2d');
-    const background = await Canvas.loadImage('./scripts/brain/assets/template.jpg');
+    const background = await Canvas.loadImage('./scripts/brain/assets/'+ img);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#000000';
     ctx.textAlign ='center';
-    // Add a-text
-    ctx.font = a_size + 'px Arial';
-    ctx.fillText(a_processed.join(''), 520, 10 + (295 - (a_size * a_processed.length)) / 2);
-    ctx.font = b_size + 'px Arial';
-    ctx.fillText(b_processed.join(''), 520, 310 + (295 - (b_size * b_processed.length)) / 2);
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'drake.jpg');
+    // Loop and add text
+    for (let i = 0; i < Math.min(parts.length,5); i++) {
+      let fsize = determineFontSize(parts[i]);
+      console.log(fsize);
+      ctx.font = fsize + 'px Arial';
+      let lines = determineLines(parts[i].split(' '), fsize.toString());
+      ctx.fillText(lines.join(''), 170, -10 + (i*215) + (295 - (fsize * lines.length)) / 2);
+    }
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'brain.jpg');
     msg.channel.send(` `, attachment);
   } else {
     msg.channel.send("do it right!");
