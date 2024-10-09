59cd4f39f6a7b4d6b648345cf2aff1ff47b35e01
the_shivers <rossbrian120@gmail.com>
2024-10-09 00:23:40 -0500

    Added model picker.

--- commands/ai.js
+++ commands/ai.js
@@ -12,41 +12,45 @@ module.exports = {
 		.setName('ai')
 		.setDescription('AI image meme.')
     .addStringOption(option => option
-      .setName('input')
+      .setName('prompt')
       .setDescription('Image prompt.')
       .setRequired(true)
+    ).addStringOption(option => option
+      .setName('model')
+      .setDescription('Image generation model to use.')
+      .addChoices({name:'sdxl-lightning-4step (very fast)', value:'bytedance/sdxl-lightning-4step:5599ed30703defd1d160a25a63321b4dec97101d98b4674bcc56e41f62f35637'})
+      .addChoices({name:'flux-schnell (fast)', value:'black-forest-labs/flux-schnell'})
+      .addChoices({name:'flux-dev', value:'black-forest-labs/flux-dev'})
     ),
 	async execute(interaction) {
 		await interaction.deferReply();
-    let input = interaction.options.getString('input');
-    if (input.length < 4) {
+    let prompt = interaction.options.getString('prompt');
+    if (prompt.length < 4) {
       interaction.editReply("You gotta give me more to work with!");
       return;
-    } else if (input.length > 1500) {
-      interaction.editReply("That's too much input! Keep it less than 1,500 characters!");
+    } else if (prompt.length > 1500) {
+      interaction.editReply("That's too much prompt! Keep it less than 1,500 characters!");
       return;
     }
-    input = {
-	"width": 1024,
-        "height": 1024,
-        "prompt": input,
-        "scheduler": "K_EULER",
-        "num_outputs": 1,
-        "guidance_scale": 0,
-        "negative_prompt": "worst quality, low quality",
-        "num_inference_steps": 4,
-	"disable_safety_checker": true
+    let model = interaction.options.getString('model') ?? 'bytedance/sdxl-lightning-4step:5599ed30703defd1d160a25a63321b4dec97101d98b4674bcc56e41f62f35637';
+    if (model == 'bytedance/sdxl-lightning-4step:5599ed30703defd1d160a25a63321b4dec97101d98b4674bcc56e41f62f35637') {
+      input = {
+        "prompt": prompt,
+	      "disable_safety_checker": true
+      }
+    } else if (model == 'black-forest-labs/flux-schnell') {
+      input = {
+        "prompt": prompt,
+	      "disable_safety_checker": true
+      }
+    } else {
+      input = {
+        "prompt": prompt,
+	      "disable_safety_checker": true
+      }
     }
     console.log("Trying this: ", {input})
-    let result = await replicate.run("bytedance/sdxl-lightning-4step:5599ed30703defd1d160a25a63321b4dec97101d98b4674bcc56e41f62f35637", { input });
-    // if (result.status != 201) {
-    //   interaction.editReply("The AI went rogue and refused!");
-    //   console.log(result)
-    //   return;
-    // }
-    // console.log(result)
-    // console.log(result.data)
-    // console.log(result.data.output)
-    interaction.editReply((`**${input.prompt}** ` + result[0]))
+    let result = await replicate.run(model, { input });
+    interaction.editReply((`**Prompt:** ${input.prompt}\n**Model:** ${model}\n` + result[0]))
 	}
 }
